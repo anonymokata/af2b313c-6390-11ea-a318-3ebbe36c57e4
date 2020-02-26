dd728f4abbd1d46ce45e93d4cddd321d58780dbf
someone <someone>
2020-02-25 19:31:43 -0500

    Adding test to ensure that our constructor properly throws an exception when our requested file isn't found.

--- tests/FileReadingTests.cpp
+++ tests/FileReadingTests.cpp
@@ -10,3 +10,9 @@ TEST(FileReadingTests, FileOpensWithoutException)
     std::shared_ptr<FileReader> f;
     ASSERT_NO_THROW(f = std::make_shared<FileReader>("./data/ProvidedWordSearch.txt"));
 }
+
+TEST(FileReadingTests, FileFailsToOpenWithException)
+{
+    std::shared_ptr<FileReader> file_object;
+    ASSERT_THROW(file_object = std::make_shared<FileReader>("./this/doesnt/exist.txt"), std::runtime_error);
+}
