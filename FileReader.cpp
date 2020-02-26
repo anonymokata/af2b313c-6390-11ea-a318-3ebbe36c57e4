18341b475f7175c3173179130df14b72a73f10c2
someone <someone>
2020-02-25 19:38:19 -0500

    Additional error checking tests for file opening. * Refactored a variable to a better, more descriptive name. * Added more tests around possible invalid strings (nullptr and empty string.)

--- FileReader.cpp
+++ FileReader.cpp
@@ -7,8 +7,13 @@
 #include <exception>
 FileReader::FileReader(const std::string &file_path)
 {
+    if (file_path.empty())
+    {
+        throw std::runtime_error("Error: You must pass a non-empty string representing a valid file path.");
+    }
     _file_path = file_path;
     _input_file_object = std::ifstream(file_path);
+
     if (!_input_file_object.good())
     {
         throw std::runtime_error("Cannot open the file: " + file_path + " requested.");
--- tests/FileReadingTests.cpp
+++ tests/FileReadingTests.cpp
@@ -7,12 +7,24 @@
 
 TEST(FileReadingTests, FileOpensWithoutException)
 {
-    std::shared_ptr<FileReader> f;
-    ASSERT_NO_THROW(f = std::make_shared<FileReader>("./data/ProvidedWordSearch.txt"));
+    std::shared_ptr<FileReader> file_object;
+    ASSERT_NO_THROW(file_object = std::make_shared<FileReader>("./data/ProvidedWordSearch.txt"));
 }
 
 TEST(FileReadingTests, FileFailsToOpenWithException)
 {
     std::shared_ptr<FileReader> file_object;
     ASSERT_THROW(file_object = std::make_shared<FileReader>("./this/doesnt/exist.txt"), std::runtime_error);
+}
+
+TEST(FileReadingTests, FileFailesToOpenNullptrNameWithException)
+{
+    std::shared_ptr<FileReader> file_object;
+    ASSERT_THROW(file_object = std::make_shared<FileReader>(nullptr), std::exception);
+}
+
+TEST(FileReadingTests, FileFailesToOpenEmptyStringWithException)
+{
+    std::shared_ptr<FileReader> file_object;
+    ASSERT_THROW(file_object = std::make_shared<FileReader>(""), std::runtime_error);
 }
