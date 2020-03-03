d7a40835823e452a68d34d1eced44d33f803db8b
someone <someone>
2020-03-02 22:35:36 -0500

    Adding yet another test to ensure that we get proper error reporting from main.

--- tests/TestAltMain.cpp
+++ tests/TestAltMain.cpp
@@ -9,6 +9,6 @@ TEST(TestAltMain, TestAltMainParameters)
     // Adding two char*'s here is a hack.
     char* args[] = {(char*)"WordSearchKata", (char*)"./data/ProvidedWordSearch.txt"};
     ASSERT_EQ(ALT_MAIN(2, args), SUCCESS);
-
-
+    char* args2[] = {(char*)"WordSearchKata", (char*)"./data/doesntexist.txt"};
+    ASSERT_EQ(ALT_MAIN(2, args2), INVALID_PARAMETERS);
 }
