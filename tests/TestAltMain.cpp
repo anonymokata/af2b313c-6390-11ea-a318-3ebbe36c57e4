583ea1820bcc194fb657776399615cad3fd1ec63
someone <someone>
2020-03-04 20:16:56 -0500

    Added setting the cout failbit, so that output doesn't affect a test runner parser.

--- tests/TestAltMain.cpp
+++ tests/TestAltMain.cpp
@@ -1,19 +1,26 @@
 /**
  * This file tests main functionality.
+ *
+ * @note In some tests, the main program will output data. This kinda makes the test output look bad, and
+ * could break a test output parser. To that end I am setting the failbit on cout any time we expect output
+ * through that stream. Afterwards, I set the goodbit in order to revert this.
  */
 #include "gtest/gtest.h"
 #include "main.hpp"
 
 /**
- * Test that the argument parisng is correct.
+ * Test that the argument parsing is correct.
  */
 TEST(TestAltMain, TestAltMainParameters)
 {
+    std::cout.setstate(std::ios_base::failbit);
+
     // Adding two char*'s here is a hack.
     char* args[] = {(char*)"WordSearchKata", (char*)"./data/ProvidedWordSearch.txt"};
     ASSERT_EQ(ALT_MAIN(2, args), SUCCESS);
     char* args2[] = {(char*)"WordSearchKata", (char*)"./data/doesntexist.txt"};
     ASSERT_EQ(ALT_MAIN(2, args2), INVALID_PARAMETERS);
+    std::cout.setstate(std::ios_base::goodbit);
 }
 
 /**
@@ -22,6 +29,8 @@ TEST(TestAltMain, TestAltMainParameters)
  */
 TEST(TestAlTMain, TestNonProvided)
 {
+    std::cout.setstate(std::ios_base::failbit);
     char* args[] = {(char*)"WordSearchKata", (char*)"./data/NextGen.txt"};
     ASSERT_EQ(ALT_MAIN(2, args), SUCCESS);
+    std::cout.setstate(std::ios_base::goodbit);
 }
